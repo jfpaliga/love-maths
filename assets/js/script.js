e2000f3ef6f79871d30b69f3a4e1ea23a1b10897
jfpaliga <>
2023-08-22 17:21:49 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubstractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -89,6 +91,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -136,4 +140,20 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivideQuestion(operand1, operand2) {
+
+    let canDivide = false;
+    while (!canDivide) {
+        if (operand1 > operand2 && operand1 % operand2 === 0) {
+            document.getElementById('operand1').textContent = operand1;
+            document.getElementById('operand2').textContent = operand2;
+            document.getElementById('operator').textContent = '/';
+            canDivide = true;
+        } else {
+            operand1 = Math.floor(Math.random() * 100) + 1;
+            operand2 = Math.floor(Math.random() * 100) + 1;
+        }
+    }
 }
